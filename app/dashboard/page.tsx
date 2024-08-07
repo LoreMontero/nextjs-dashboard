2eff2e00633a46618c4ee6de678baa11ce573f0d
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air-3.local>
2024-08-07 17:09:05 -0400

    Added imports to dashboard page file

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@app/ui/dahsboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@app/ui/fonts';
+
+export default async function Page() {
     return <p>Dashboard Page</p>;
 }
